4db4bd4be5e4b1ca3b89fe270578f2a964a97dce
Filip <kasprzyckifilip11@gmail.com>
2025-11-09 19:01:51 +0100

    Dodanie nowych danych

--- main.R
+++ main.R
@@ -2,5 +2,7 @@
 library(readxl)
 dane1 <- read_excel("Tobacco_consumption_HLTH2022-2.xlsx",sheet = "T1",)
 OdsetekPalacy <- dane1[, 1:2]
-
-
+OdsetekPalacy <- OdsetekPalacy[order(OdsetekPalacy[[1]]), ]
+dane2 <- read_excel("Europe_Population.xlsx")
+PopulacjaKraju<- dane2[, 1:2]
+PopulacjaKraju<- PopulacjaKraju[order(PopulacjaKraju[[1]]), ]
