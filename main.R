24425b5bc18bbcd9b479040b71c73af87c8c95bb
Filip <kasprzyckifilip11@gmail.com>
2025-11-12 14:24:48 +0100

    Dane

--- main.R
+++ main.R
@@ -11,5 +11,37 @@ PKBKraju <- dane3[, 1:2]
 dane4 <- read_excel("Direct-taxes.xlsx", skip = 5, col_names = FALSE, sheet="Table 13")
 AkcyzaPKB <- dane4[, c(1,5)] #akcyza jako porcent pkb kraju
 AkcyzaPKB <- AkcyzaPKB[1:(nrow(AkcyzaPKB) - 6), ]
+
+colnames(AkcyzaPKB) <- c("Country", "ProcentPKB")
+Kraje <- intersect(
+  intersect(OdsetekPalacy$Country, PopulacjaKraju$Country),
+  intersect(PKBKraju$Country, AkcyzaPKB$Country)
+)
+
 Kraje=intersect(OdesetekPalcy$Country, )
 
+
+
+OdsetekPalacy <- OdsetekPalacy[OdsetekPalacy$Country %in% Kraje, ]
+PopulacjaKraju <- PopulacjaKraju[PopulacjaKraju$Country %in% Kraje, ]
+PKBKraju <- PKBKraju[PKBKraju$Country %in% Kraje, ]
+AkcyzaPKB <- AkcyzaPKB[AkcyzaPKB$Country %in% Kraje, ]
+
+OdsetekPalacy <- OdsetekPalacy[order(OdsetekPalacy$Country), ]
+PopulacjaKraju <- PopulacjaKraju[order(PopulacjaKraju$Country), ]
+PKBKraju <- PKBKraju[order(PKBKraju$Country), ]
+AkcyzaPKB <- AkcyzaPKB[order(AkcyzaPKB$Country), ]
+
+rm(dane1)
+rm(dane2)
+rm(dane3)
+rm(dane4)
+
+DanePolaczone <- data.frame(
+  Country = OdsetekPalacy$Country,
+  OdsetekPalacy = OdsetekPalacy[[2]],
+  Populacja = PopulacjaKraju[[2]],
+  PKB_per_capita = PKBKraju[[2]],
+  Akcyza_procent_PKB = AkcyzaPKB[[2]]
+)
+print("")
